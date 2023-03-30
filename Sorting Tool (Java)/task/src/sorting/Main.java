9cc1de8b900dcfe594a900b6bd61954d198c81f2
Varganum <nikolai.sobolev@gmail.com>
2023-03-30 12:38:22 +0600

    Final stage 6

--- Sorting Tool (Java)/task/src/sorting/Main.java
+++ Sorting Tool (Java)/task/src/sorting/Main.java
@@ -1,5 +1,6 @@
 package sorting;
 
+import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -7,9 +8,12 @@ import java.util.stream.Collectors;
 public class Main {
 
     private static String[] typeOfDataAndSorting = new String[2];
-
+    private static File inputFile;
+    private static File outputFile;
     private static final String SORTING_TYPE_MARKER = "-sortingType";
     private static final String DATA_TYPE_MARKER = "-dataType";
+    private static final String INPUT_FILE_MARKER = "-inputFile";
+    private static final String OUTPUT_FILE_MARKER = "-outputFile";
     private static final String SORT_NATURAL_PARAMETER = "natural";
     private static final String SORT_BY_COUNT_PARAMETER = "byCount";
     private static final String LONG_TYPE_PARAMETER = "long";
@@ -21,8 +25,6 @@ public class Main {
         boolean isArgumentsCorrect;
 
         isArgumentsCorrect = parseArguments(args);
-
-        //System.out.println(typeOrOperationChosen);
 
         if (isArgumentsCorrect) {
             switch (typeOfDataAndSorting[1]) {
@@ -47,11 +49,14 @@ public class Main {
 
 
     private static void operateWords(String sortingType) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner;
+        PrintStream outputFileWriter;
         TreeMap<String, Long> wordsSortedNaturally = new TreeMap<>(Comparator.naturalOrder());
 
         long newQuantity;
         int wordsCounter = 0;
+
+        scanner = getScanner();
 
         while (scanner.hasNext()) {
             String word = scanner.next();
@@ -64,6 +69,12 @@ public class Main {
                 wordsSortedNaturally.put(word, 1L);
             }
         }
+
+        scanner.close();
+
+        boolean isOutputFile = Objects.nonNull(Main.outputFile);
+
+        outputFileWriter = setOutputStream(isOutputFile);
 
         //Output results according to sorting type chosen
         System.out.println("Total words: " + wordsCounter + ".");
@@ -78,19 +89,27 @@ public class Main {
             Set<String> wordsSortedByCount = wordsSortedNaturally.keySet().stream().sorted(Comparator.comparingLong(wordsSortedNaturally::get)).collect(Collectors.toCollection(LinkedHashSet::new));
 
             //output sorted byCount elements
-            //System.out.println();
             for (String word : wordsSortedByCount) {
                 System.out.println(word + ": " + wordsSortedNaturally.get(word) + " time(s), " +
-                        Math.round(100 * (double) wordsSortedNaturally.get(word) / wordsCounter) + "%");
+                            Math.round(100 * (double) wordsSortedNaturally.get(word) / wordsCounter) + "%");
             }
+        }
+
+        if (Objects.nonNull(outputFileWriter)) {
+            outputFileWriter.close();
         }
     }
 
+
+
     private static void operateLines(String sortingType) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner;
+        PrintStream outputFileWriter;
         TreeMap<String, Long> linesSortedNaturally = new TreeMap<>(Comparator.naturalOrder());
         long newQuantity;
         int linesCounter = 0;
+
+        scanner = getScanner();
 
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
@@ -104,7 +123,13 @@ public class Main {
             }
         }
 
-        //Output lines according to sorting types
+        scanner.close();
+
+        boolean isOutputFile = Objects.nonNull(Main.outputFile);
+
+        outputFileWriter = setOutputStream(isOutputFile);
+
+        //Output lines to console according to sorting types
         System.out.println("Total lines: " + linesCounter + ".");
 
         if (Main.SORT_NATURAL_PARAMETER.equals(sortingType)) {
@@ -121,20 +146,26 @@ public class Main {
             //output sorted byCount elements
             for (String line : linesSortedByCount) {
                 System.out.println(line + ": " + linesSortedNaturally.get(line) + " time(s), " +
-                        (int) (100 * linesSortedNaturally.get(line) / linesCounter) + "%");
+                        Math.round(100 * (double) linesSortedNaturally.get(line) / linesCounter) + "%");
             }
         }
 
+        if (Objects.nonNull(outputFileWriter)) {
+            outputFileWriter.close();
+        }
     }
 
     private static void operateLongs(String sortingType) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner;
+        PrintStream outputFileWriter;
         TreeMap<Long, Long> numbers = new TreeMap<>();
         long newQuantity;
         int numbersCounter = 0;
         String nextString = null;
         long number;
+
+        scanner = getScanner();
 
         while (scanner.hasNext()) {
             try {
@@ -153,7 +184,13 @@ public class Main {
             }
         }
 
-        //Output results according to sorting type chosen
+        scanner.close();
+
+        boolean isOutputFile = Objects.nonNull(Main.outputFile);
+
+        outputFileWriter = setOutputStream(isOutputFile);
+
+        //Output results to console according to sorting type chosen
         System.out.println("Total numbers: " + numbersCounter + ".");
 
         if (Main.SORT_NATURAL_PARAMETER.equals(sortingType)) {
@@ -168,13 +205,44 @@ public class Main {
             Set<Long> numbersSortedByCount = numbers.keySet().stream().sorted(Comparator.comparingLong(numbers::get)).collect(Collectors.toCollection(LinkedHashSet::new));
 
             //output sorted byCount elements
-            //System.out.println();
             for (Long num : numbersSortedByCount) {
                 System.out.println(num + ": " + numbers.get(num) + " time(s), " +
-                        Math.round(100 * (double) numbers.get(num) / numbersCounter) + "%");
+                            Math.round(100 * (double) numbers.get(num) / numbersCounter) + "%");
             }
         }
 
+        if (Objects.nonNull(outputFileWriter)) {
+            outputFileWriter.close();
+        }
+    }
+
+
+    private static Scanner getScanner() {
+        Scanner inputScanner = null;
+        if (Objects.nonNull(Main.inputFile) && Main.inputFile.exists()) {
+            try {
+                inputScanner = new Scanner(Main.inputFile);
+            } catch (FileNotFoundException exception) {
+                System.out.println("Input file is not found.");
+            }
+        } else {
+            inputScanner = new Scanner(System.in);
+        }
+        return inputScanner;
+    }
+
+    private static PrintStream setOutputStream(boolean isOutputFile) {
+        PrintStream result = null;
+        if (isOutputFile) {
+            try {
+                //if output file is provided - create new PrintStream and set System out stream to this mew PrintStream
+                result = new PrintStream(Main.outputFile);
+                System.setOut(result);
+            } catch (IOException exception) {
+                System.out.println("Some troubles with output file.");
+            }
+        }
+        return result;
     }
 
     /* Method for parsing program arguments (Stage 4 of project).
@@ -218,119 +286,27 @@ public class Main {
         } else {
             result[1] = Main.WORD_TYPE_PARAMETER;
         }
-        Main.typeOfDataAndSorting = result;
+
+        if (allArguments.contains(Main.INPUT_FILE_MARKER)) {
+            int index = allArguments.indexOf(Main.INPUT_FILE_MARKER) + 1;
+            Main.inputFile = new File(allArguments.get(index));
+        }
+
+        if (allArguments.contains(Main.OUTPUT_FILE_MARKER)) {
+            int index = allArguments.indexOf(Main.OUTPUT_FILE_MARKER) + 1;
+            Main.outputFile = new File(allArguments.get(index));
+        }
 
         for (String arg : allArguments) {
             if (arg.matches("-.*") &&
-                    (!Main.DATA_TYPE_MARKER.equals(arg) & !Main.SORTING_TYPE_MARKER.equals(arg))) {
+                    (!Main.DATA_TYPE_MARKER.equals(arg) & !Main.SORTING_TYPE_MARKER.equals(arg) &
+                            !Main.INPUT_FILE_MARKER.equals(arg) & !Main.OUTPUT_FILE_MARKER.equals(arg))) {
                 System.out.println("\"" + arg + "\" is not a valid parameter. It will be skipped.");
             }
         }
 
+        Main.typeOfDataAndSorting = result;
         return isArgumentsCorrect;
     }
-
-    /*
-        //Methods from stages 1 - 3 of project//
-
-    private static void operateLongs() {
-
-        Scanner scanner = new Scanner(System.in);
-        TreeMap<Long, Long> numbers = new TreeMap<>();
-        long newQuantity;
-        int numbersCounter = 0;
-
-        while (scanner.hasNextLong()) {
-            long number = scanner.nextLong();
-            numbersCounter++;
-            // write your code here
-            if (numbers.containsKey(number)) {
-                newQuantity = numbers.get(number) + 1;
-                numbers.put(number, newQuantity);
-            } else {
-                numbers.put(number, 1L);
-            }
-        }
-        System.out.println("Total numbers: " + numbersCounter + ".");
-        System.out.println("The greatest number: " + numbers.lastKey() + "(" +
-                numbers.get(numbers.lastKey()) + " time(s)," +
-                (int) (100 * numbers.get(numbers.lastKey()) / numbersCounter) + "%).");
-    }
-
-    private static void operateLines() {
-        Scanner scanner = new Scanner(System.in);
-        TreeMap<String, Long> lines = new TreeMap<>(Comparator.comparingInt(String::length).thenComparing(Comparator.naturalOrder()));
-        long newQuantity;
-        int linesCounter = 0;
-
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            linesCounter++;
-
-            if (lines.containsKey(line)) {
-                newQuantity = lines.get(line) + 1;
-                lines.put(line, newQuantity);
-            } else {
-                lines.put(line, 1L);
-            }
-        }
-        System.out.println("Total lines: " + linesCounter + ".");
-        System.out.println("The longest line:\n" + lines.lastKey() + "\n(" +
-                lines.get(lines.lastKey()) + " time(s), " +
-                (int) (100 * lines.get(lines.lastKey()) / linesCounter) + "%).");
-    }
-
-    private static void sortIntegers() {
-
-        Scanner scanner = new Scanner(System.in);
-        TreeMap<Long, Long> numbers = new TreeMap<>();
-        long newQuantity;
-        int numbersCounter = 0;
-
-        while (scanner.hasNextLong()) {
-            long number = scanner.nextLong();
-            numbersCounter++;
-            // write your code here
-            if (numbers.containsKey(number)) {
-                newQuantity = numbers.get(number) + 1;
-                numbers.put(number, newQuantity);
-            } else {
-                numbers.put(number, 1L);
-            }
-        }
-        System.out.println("Total numbers: " + numbersCounter + ".");
-        System.out.print("Sorted data:");
-
-        for (Long num : numbers.keySet()) {
-            for (long i = 0; i < numbers.get(num); i++) {
-                System.out.print(" " + num);
-            }
-        }
-    }
-
-    private static void operateWords() {
-        Scanner scanner = new Scanner(System.in);
-        TreeMap<String, Long> words = new TreeMap<>(Comparator.comparingInt(String::length).thenComparing(Comparator.naturalOrder()));
-        long newQuantity;
-        int wordsCounter = 0;
-
-        while (scanner.hasNext()) {
-            String word = scanner.next();
-            wordsCounter++;
-
-            if (words.containsKey(word)) {
-                newQuantity = words.get(word) + 1;
-                words.put(word, newQuantity);
-            } else {
-                words.put(word, 1L);
-            }
-        }
-        System.out.println("Total words: " + wordsCounter + ".");
-        System.out.println("The longest word: " + words.lastKey() + " (" +
-                words.get(words.lastKey()) + " time(s), " +
-                (int) (100 * words.get(words.lastKey()) / wordsCounter) + "%).");
-    }
-
-    */
 
 }
